b890e4195f8a4538df4cb51804806ec8bde7a069
SH-Yoo <ads137485@jbnu.ac.kr>
2024-07-18 11:29:35 +0900

    특정 tag만 추출하는 함수를 추가함. 재귀적으로 DFS순회하며 탐색하는데, 게층 LEVEL이 2라서 시간 측면서에 효율적인지 모르겠음. 근데 계층구조가 크지 않아서 반복문이랑 비교해도 크게 차이나지 않을듯? 근데 중첩으로 순회하거나 반복하는 부분이 있을 거 같은데 이 부분을 조금 수정해야할듯.

--- data_processing_ver3.py
+++ data_processing_ver3.py
@@ -7,8 +7,8 @@ root = tree.getroot()
 # 특정 범위의 요소 추출
 def extract_range_texts_in_item(item, start_tag, end_tag):
     texts = []
-    stack = [item] #tag node들을 명시적으로 관리하기 위해 stack과 반복문을 이용하여 추출.
-    start_found = False 
+    stack = [item]  # tag node들을 명시적으로 관리하기 위해 stack과 반복문을 이용하여 추출.
+    start_found = False
 
     while stack:
         node = stack.pop()
@@ -18,13 +18,31 @@ def extract_range_texts_in_item(item, start_tag, end_tag):
             texts.append(node.text.strip())
         if node.tag == end_tag:
             break
-        stack.extend(reversed(node)) 
+        stack.extend(reversed(node))
 
     return texts
 
-# 모든 <item> 요소에서 <title>부터 <comment_html>까지 텍스트 추출
+# 특정 태그의 텍스트 추출
+def extract_texts_in_item(item, tag):
+    texts = []
+    #item.iter(tag): item.iter(tag): 이 메서드는 item 요소 내에서 주어진 tag를 가진 모든 자식 요소를 깊이 우선 순회(DFS) 방식으로 찾습니다.
+    for node in item.iter(tag):
+        if node.text:
+            texts.append(node.text.strip())
+        else:
+            texts.append('None')
+    return texts
+
+""" # 모든 <item> 요소에서 <title>부터 <comment_html>까지 텍스트 추출
 for item in root.findall('.//item'):
     texts = extract_range_texts_in_item(item, 'title', 'comment_html')
     print("Extracted texts from <title> to <comment_html>:")
     for text in texts:
+        print(text) """
+
+# 예시: <item> 요소에서 모든 <link> 태그의 텍스트 추출
+for item in root.findall('.//item'):
+    link_texts = extract_texts_in_item(item, 'comment_html')
+    print("Extracted texts from <link>:")
+    for text in link_texts:
         print(text)
