7306517f0ee21c33b67b183f5719fbf0388280df
SH-Yoo <ads137485@jbnu.ac.kr>
2024-07-18 13:48:50 +0900

    특정 tag node만을 추출하도록 하되, 보다 좋은 성능으로 함수를 재구성, 또한 oasst2 filed 값과 html parsing에 필요한 클래스값 저장

--- data_processing_ver3.py
+++ data_processing_ver3.py
@@ -1,11 +1,30 @@
+import pandas as pd
+from bs4 import BeautifulSoup
+import logging
+import os
+import argparse
+import platform
+import requests
 from lxml import etree
 
-# XML 파일 파싱
-tree = etree.parse('xml/sample1.xml')
-root = tree.getroot()
+# Set up logging
+logging.basicConfig(filename='parsing_link_test.log', level=logging.INFO,
+                    format='%(asctime)s - %(levelname)s - %(message)s')
 
-# 특정 범위의 요소 추출
+
+
 def extract_range_texts_in_item(item, start_tag, end_tag):
+    """ 특정 범위의 요소 추출
+
+    
+    Args:
+        item (_type_): _description_
+        start_tag (_type_): _description_
+        end_tag (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
     texts = []
     stack = [item]  # tag node들을 명시적으로 관리하기 위해 stack과 반복문을 이용하여 추출.
     start_found = False
@@ -22,27 +41,51 @@ def extract_range_texts_in_item(item, start_tag, end_tag):
 
     return texts
 
-# 특정 태그의 텍스트 추출
-def extract_texts_in_item(item, tag):
+
+   
+def extract_texts_from_items(root, tag):
+    """특정 태그의 텍스트 추출
+
+    Args:
+        root (_type_): _description_
+        tag (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
     texts = []
-    #item.iter(tag): item.iter(tag): 이 메서드는 item 요소 내에서 주어진 tag를 가진 모든 자식 요소를 깊이 우선 순회(DFS) 방식으로 찾습니다.
-    for node in item.iter(tag):
-        if node.text:
-            texts.append(node.text.strip())
-        else:
-            texts.append('None')
+    
+    for item in root.findall('.//item'):
+        for elem in item.findall(tag):
+            if elem.text:
+                texts.append(elem.text.strip())
+            else:
+                texts.append('None')
     return texts
 
-""" # 모든 <item> 요소에서 <title>부터 <comment_html>까지 텍스트 추출
-for item in root.findall('.//item'):
-    texts = extract_range_texts_in_item(item, 'title', 'comment_html')
-    print("Extracted texts from <title> to <comment_html>:")
-    for text in texts:
-        print(text) """
 
-# 예시: <item> 요소에서 모든 <link> 태그의 텍스트 추출
-for item in root.findall('.//item'):
-    link_texts = extract_texts_in_item(item, 'comment_html')
-    print("Extracted texts from <link>:")
-    for text in link_texts:
-        print(text)
+
+
+def main():
+
+    # XML 파일 파싱
+    tree = etree.parse('xml/sample1.xml')
+    root = tree.getroot()
+
+    
+# 추출할 태그
+tag_to_extract = 'comment_html'
+
+# 텍스트 추출
+extracted_texts = extract_texts_from_items(root, tag_to_extract)
+
+# 결과 출력
+print(f"Extracted texts from <{tag_to_extract}>:")
+for text in extracted_texts:
+    print('comment_html:',text)     
+
+
+
+    
+if __name__ == "__main__":
+    main()
