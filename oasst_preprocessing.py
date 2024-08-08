23fdaa07243f8d98d26a1ea02b21b4c3300aeb39
SH-Yoo <ads137485@jbnu.ac.kr>
2024-08-08 09:18:02 +0900

    readMe 업데이트

--- oasst_preprocessing.py
+++ oasst_preprocessing.py
@@ -1,21 +1,24 @@
-# -*- coding: utf-8 -*-
-import pandas as pd
-from kiwipiepy import Kiwi, Match
-from kiwipiepy.utils import Stopwords
+import re
+from kiwipiepy import Kiwi
 
-
-#kiwi 생성자 정보 Kiwi(num_workers=0, model_path=None, load_default_dict=True, integrate_allomorph=True, model_type='knlm', typos=None, typo_cost_threshold=2.5)
-
+# Kiwi 객체 생성
 kiwi = Kiwi()
-sentences = "안녕하세요. 최선을 다하는 이희범 변호사입니다. 1. 우선 상속인, 상속재산이 있었는지 등에 대한 판단이 필요해 보입니다. 2. 동사무소에 방문하여 상속재산 원스톱 서비스 등을 신청하시길 바랍니다.  더 궁금한 점이 있으시면 언제든지 연락 주시면 친절히 상담 드리도록 하겠습니다. [라미 법률사무소 대표 변호사 / 이희범 배상] - 대한 변호사 협회 등록 형사전문 변호사/ 공인 도로교통사고 감정사 - 대한 변호사 협회 등록 학교폭력 전문변호사 / 공인 가맹거래사 - 한 권에 담은 음주운전 사건 사고처리 저자 - 한 권에 담은 개인회생 사건 사고처리 저자 - 한 권에 담은 학교폭력의 바이블 저자"
+
+# 분석할 문장
+text = "안녕하세요. 변호사 이희범입니다. 법무법인 XYZ도 있습니다. 자세한 사항은 https://example.com에서 확인하세요. 서울대 경영학 출신 변호사 홍대범입니다."
+
+# URL 패턴 정의, \S+: 공백이 아닌 문자 하나 이상을 의미합니다. URL의 나머지 부분을 매칭합니다.
+url_pattern = re.compile(r'https?://\S+|www\.\S+')
+name_pattern =  re.compile(r'\S*변호사|변호사\S*')
 
 
-# 토큰화
-tokens = kiwi.tokenize(sentences)
 
-# NNG만 추출
-nng_tokens = [token[0] for token in tokens if token[1] == 'NNG']
+# URL 제거
+clean_text = url_pattern.sub('', text)
 
-# 결과 출력
-print(f"NNG Tokens: {nng_tokens}")
-#print(f"Tokens: {tokens}")
+# 이름 관련 패턴 제거
+clean_text = name_pattern.sub('', clean_text)
+
+
+print("URL 제거 후 텍스트:")
+print(clean_text)
