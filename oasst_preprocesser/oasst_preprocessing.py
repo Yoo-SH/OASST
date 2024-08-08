455c816d76bcaf039d3aac058b9940aa4f43b7a1
SH-Yoo <ads137485@jbnu.ac.kr>
2024-08-08 17:10:40 +0900

    ulr을 지우는 정규식과, text_header와 text_footer를 지우는 코드를 추가

--- oasst_preprocesser/oasst_preprocessing.py
+++ oasst_preprocesser/oasst_preprocessing.py
@@ -2,48 +2,55 @@ import duckdb
 import pandas as pd
 import re
 
-# 데이터를 매칭하고 content에서 company_name 제거
-def remove_company_name(content, company_name): 
-    if pd.isna(company_name): # NaN 값이라면 
-        return content
-    pattern = re.compile(re.escape(company_name)) # 정규표현식 문자 이스케이프 처리 후, 정규표현식 패턴 생성
-    return pattern.sub('', content) # content에서 company_name을 빈 문자열로 대체
+# 데이터를 매칭하고 text에서 삭제 텍스트 제거
+def remove_text(text, delete_text, delete_hi): 
+    if pd.isna(delete_text): # NaN 값이라면 
+        delete_text = ''
+    if pd.isna(delete_hi): # NaN 값이라면 
+        delete_hi = ''
+           # URL 정규 표현식 패턴
+           
+    url_pattern = re.compile(r'http[s]?://\S+|www\.\S+')
+    pattern_text = re.compile(re.escape(delete_text)) # 정규표현식 문자 이스케이프 처리 후, 정규표현식 패턴 생성
+    pattern_hi = re.compile(re.escape(delete_hi)) # 정규표현식 문자 이스케이프 처리 후, 정규표현식 패턴 생성
+
+    text = url_pattern.sub('', text) # text에서 URL을 빈 문자열로 대체
+    text = pattern_text.sub('', text) # text에서 delete_text를 빈 문자열로 대체
+    text = pattern_hi.sub('', text) # text에서 delete_hi를 빈 문자열로 대체
+    return text
 
 # Excel 파일 읽기
-input_file_path = 'oasst_lawtalk_상담사례_20240807.xlsx'
-db_file_path = '로톡_변호사_delete.xlsx'
+oasst_file_path = 'oasst_lawtalk_상담사례_20240807.xlsx'
+filter_file_path = '로톡_변호사_delete_dictionary.xlsx'
 
 # Excel 파일 읽기
-db_file_df = pd.read_excel(db_file_path)
-input_file_df = pd.read_excel(input_file_path)
+oasst_file = pd.read_excel(oasst_file_path)
+filter_file = pd.read_excel(filter_file_path)
 
 # DuckDB 연결
 conn = duckdb.connect('my_database.duckdb')
 
-# DataFrame을 테이블로 로드
-conn.register('db_file_df', db_file_df)
-conn.register('input_file_df', input_file_df)
-
 # 이미 DB에 테이블이 존재하면 삭제
-conn.execute("DROP TABLE IF EXISTS lawyer_data")
-conn.execute("DROP TABLE IF EXISTS input_data")
+conn.execute("DROP TABLE IF EXISTS oasst_table")
+conn.execute("DROP TABLE IF EXISTS filter_table")
 
-conn.execute("CREATE TABLE lawyer_data AS SELECT * FROM db_file_df")
-conn.execute("CREATE TABLE input_data AS SELECT * FROM input_file_df")
+# DataFrame을 테이블로 로드
+conn.register('oasst_table', oasst_file)
+conn.register('filter_table', filter_file)
 
 # 특정 쿼리 실행
-db_file_query_result = conn.execute("SELECT lawyer_name, delete_text FROM lawyer_data").fetchdf()
-input_file_query_result = conn.execute("SELECT * FROM input_data").fetchdf()
+filter_file_query_result = conn.execute("SELECT lawyer_name, delete_text, delete_hi FROM filter_table").fetchdf()
+oasst_table_query_result = conn.execute("SELECT * FROM oasst_table").fetchdf()
 
 # 매칭된 데이터를 처리하여 새로운 content 생성
-for i, row in input_file_query_result.iterrows():   # iterrows는 (인덱스, Series) 쌍을 반환, Series는 각 행의 데이터를 포함.
-    matched_rows = db_file_query_result[db_file_query_result['lawyer_name'] == row['변호사명']]
+for i, row in oasst_table_query_result.iterrows():   # iterrows는 (인덱스, Series) 쌍을 반환, Series는 각 행의 데이터를 포함.
+    matched_rows = filter_file_query_result[filter_file_query_result['lawyer_name'] == row['변호사명']]
     for _, match in matched_rows.iterrows(): # 매칭된 각 행을 다시 iterrows()로 순회. 행의 인덱스는 사용하지 않음.
-        input_file_query_result.at[i, 'text'] = remove_company_name(input_file_query_result.at[i, 'text'], match['delete_text']) # input_file_query_result.at[i, 'text']에 이 새로운 문자열을 할당하여 content를 업데이트
+        oasst_table_query_result.at[i, 'text'] = remove_text(oasst_table_query_result.at[i, 'text'], match['delete_text'], match['delete_hi']) # oasst_table_query_result.at[i, 'text']에 이 새로운 문자열을 할당하여 text를 업데이트
 
 # 결과 확인
-print(input_file_query_result.head())
+print(oasst_table_query_result.head())
 
 # 결과를 Excel 파일로 저장
 output_file_path = 'output_file.xlsx'
-input_file_query_result.to_excel(output_file_path, index=False)
+oasst_table_query_result.to_excel(output_file_path, index=False)
