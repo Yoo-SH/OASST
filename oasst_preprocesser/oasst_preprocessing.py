cabfb1a55e66236e3d47bc4f646deb76a9852169
SH-Yoo <ads137485@jbnu.ac.kr>
2024-08-08 17:31:17 +0900

    특정 지역명(고유명사)를 지우는 코드 추가

--- oasst_preprocesser/oasst_preprocessing.py
+++ oasst_preprocesser/oasst_preprocessing.py
@@ -2,14 +2,20 @@ import duckdb
 import pandas as pd
 import re
 
+# 특정 지역명을 제거하는 정규 표현식 패턴 생성
+def create_location_pattern(locations):
+    escaped_locations = [re.escape(loc) for loc in locations]
+    pattern = '|'.join(escaped_locations)
+    return re.compile(pattern)
+
 # 데이터를 매칭하고 text에서 삭제 텍스트 제거
-def remove_text(text, delete_text, delete_hi): 
+def remove_text(text, delete_text, delete_hi, location_pattern): 
     if pd.isna(delete_text): # NaN 값이라면 
         delete_text = ''
     if pd.isna(delete_hi): # NaN 값이라면 
         delete_hi = ''
-           # URL 정규 표현식 패턴
-           
+        
+    # URL 정규 표현식 패턴
     url_pattern = re.compile(r'http[s]?://\S+|www\.\S+')
     pattern_text = re.compile(re.escape(delete_text)) # 정규표현식 문자 이스케이프 처리 후, 정규표현식 패턴 생성
     pattern_hi = re.compile(re.escape(delete_hi)) # 정규표현식 문자 이스케이프 처리 후, 정규표현식 패턴 생성
@@ -17,6 +23,7 @@ def remove_text(text, delete_text, delete_hi):
     text = url_pattern.sub('', text) # text에서 URL을 빈 문자열로 대체
     text = pattern_text.sub('', text) # text에서 delete_text를 빈 문자열로 대체
     text = pattern_hi.sub('', text) # text에서 delete_hi를 빈 문자열로 대체
+    text = location_pattern.sub('', text) # text에서 지역명을 빈 문자열로 대체
     return text
 
 # Excel 파일 읽기
@@ -39,14 +46,18 @@ conn.register('oasst_table', oasst_file)
 conn.register('filter_table', filter_file)
 
 # 특정 쿼리 실행
-filter_file_query_result = conn.execute("SELECT lawyer_name, delete_text, delete_hi FROM filter_table").fetchdf()
+filter_file_query_result = conn.execute("SELECT lawyer_name, delete_header, delete_header FROM filter_table").fetchdf()
 oasst_table_query_result = conn.execute("SELECT * FROM oasst_table").fetchdf()
 
+# 제거할 지역명 목록 정의
+locations_to_remove = ['대전', '서울', '부산', '완주', '울산', '대한'] # 예시: 추가할 지역명 목록
+location_pattern = create_location_pattern(locations_to_remove)
+
 # 매칭된 데이터를 처리하여 새로운 content 생성
-for i, row in oasst_table_query_result.iterrows():   # iterrows는 (인덱스, Series) 쌍을 반환, Series는 각 행의 데이터를 포함.
+for i, row in oasst_table_query_result.iterrows():
     matched_rows = filter_file_query_result[filter_file_query_result['lawyer_name'] == row['변호사명']]
-    for _, match in matched_rows.iterrows(): # 매칭된 각 행을 다시 iterrows()로 순회. 행의 인덱스는 사용하지 않음.
-        oasst_table_query_result.at[i, 'text'] = remove_text(oasst_table_query_result.at[i, 'text'], match['delete_text'], match['delete_hi']) # oasst_table_query_result.at[i, 'text']에 이 새로운 문자열을 할당하여 text를 업데이트
+    for _, match in matched_rows.iterrows():
+        oasst_table_query_result.at[i, 'text'] = remove_text(oasst_table_query_result.at[i, 'text'], match['delete_header'], match['delete_header'], location_pattern)
 
 # 결과 확인
 print(oasst_table_query_result.head())
