a5899e7ada453a95517cdebf0cd712a50903c410
kincjf <sinho0689@gmail.com>
2024-08-12 02:00:13 +0900

    build: change flake8 setting

--- scripts/check_gitlint_install_hook.py
+++ scripts/check_gitlint_install_hook.py
@@ -5,13 +5,15 @@ import sys
 
 def is_command_available(command):
     """Check if a command is available on the system."""
+    ## check windows and linux both
     check_command = ['where', command] if os.name == 'nt' else ['which', command]
     result = subprocess.run(check_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     return result.returncode == 0
 
 
-def is_gitlint_hook_set():
+def is_commitmsg_hook_set():
     """Check if the gitlint hook is set up in the commit-msg file."""
+    ##
     commit_msg_hook_path = '.git/hooks/commit-msg'
     if not os.path.isfile(commit_msg_hook_path):
         return False
@@ -31,7 +33,7 @@ def main():
         print("gitlint could not be found. Please install gitlint first.")
         sys.exit(1)
 
-    if not is_gitlint_hook_set():
+    if not is_commitmsg_hook_set():
         print("commit-msg hook is not set up. Setting it up now...")
         if install_gitlint_hook():
             print("gitlint install-hook set up successfully.")
@@ -39,7 +41,8 @@ def main():
             print("Failed to set up gitlint install-hook.")
             sys.exit(1)
     else:
-        print("gitlint install-hook is already set up.")
+        # print("gitlint install-hook is already set up.")
+        print("commit-msg is already set up.")
 
 
 if __name__ == "__main__":
