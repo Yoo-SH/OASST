981845e95411d7459a46abd7cda162e1e273eaea
SH-Yoo <ads137485@jbnu.ac.kr>
2024-08-13 11:02:27 +0900

    feat: pandas와 pyarrow를 적용시켜 데이터 전처리 속도 향상시키는 코드 추가

--- oasst_preprocesser/oasst_morpheme_deleter.py
+++ oasst_preprocesser/oasst_morpheme_deleter.py
@@ -5,7 +5,7 @@ from kiwipiepy import Kiwi
 
 # Excel 파일 읽기
 oasst_file_path = 'oasst_lawtalk_상담사례_20240807.xlsx'
-filter_file_path = 'dict.xlsx'
+filter_file_path = '지역명.xlsx'
 
 oasst_file = pd.read_excel(oasst_file_path)
 filter_file = pd.read_excel(filter_file_path)
@@ -14,8 +14,8 @@ filter_file = pd.read_excel(filter_file_path)
 conn = duckdb.connect('my_database.duckdb')
 
 # 기존 테이블이 존재하면 삭제
-conn.execute("DROP TABLE IF EXISTS oasst_table") 
-conn.execute("DROP TABLE IF EXISTS filter_table") 
+conn.execute("DROP TABLE IF EXISTS oasst_table")
+conn.execute("DROP TABLE IF EXISTS filter_table")
 
 # DataFrame을 테이블로 저장
 conn.execute("CREATE TABLE oasst_table AS SELECT * FROM oasst_file")
@@ -29,10 +29,10 @@ filter_pattern = f"\\s*({'|'.join(map(re.escape, filter_to_remove))})\\s*"
 update_query = f"""
     UPDATE oasst_table
     SET text = regexp_replace(
-                   regexp_replace(oasst_table.text,                       
+                   regexp_replace(oasst_table.text,
                     '{filter_pattern}', '', 'g'),
                'http[s]?://\\S+|www\\.\\S+', '', 'g')
-    WHERE role = 'assistant' or 'prompter' 
+    WHERE role = 'assistant' or role = 'prompter'
 """
 
 conn.execute(update_query)
@@ -43,10 +43,12 @@ text_df = conn.execute("SELECT text FROM oasst_table").fetchdf()
 # 형태소 분석기 초기화
 kiwi = Kiwi()
 
+
 # 형태소 분석 함수 정의
 def analyze_text(text):
     result = kiwi.analyze(text)
     return result[0][0]  # 첫 번째 문장과 첫 번째 분석 결과 반환
+
 
 # 데이터프레임의 각 텍스트에 대해 형태소 분석 수행
 text_df['morphemes'] = text_df['text'].apply(analyze_text)
